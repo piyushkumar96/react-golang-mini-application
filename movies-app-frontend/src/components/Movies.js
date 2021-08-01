a5a94e680ad66e4fd0adc03716858f5067678dbd
Piyush Kumar <iyu1kor@bosch.com>
2021-08-01 17:53:40 +0530

    Added code to connect frontend componenet with backend and pull data using backend APIs

--- movies-app-frontend/src/components/Movies.js
+++ movies-app-frontend/src/components/Movies.js
@@ -2,29 +2,51 @@ import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
-  state = { movies: [] };
+  state = { movies: [], isLoaded: false, error: null };
 
   componentDidMount() {
-    this.setState({
-      movies: [
-        { id: 1, title: "The Shawshank Redemption", runtime: 142 },
-        { id: 2, title: "Theory of Everything", runtime: 122 },
-      ],
-    });
+    fetch("http://localhost:4000/v1/movies")
+        .then((response) => {
+            if (response.status !== 200) {
+                let err = Error;
+                err.message = "Invalid response code " + response.status
+                this.setState({error: err});
+            }
+
+            return response.json()
+        })
+        .then((json) => this.setState({
+          movies: json.movies,
+          isLoaded: true
+        },(error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        }))
+
   }
 
   render() {
-    return (
-      <Fragment>
-        <h1>Choose a movie</h1>
-        <ul>
-          {this.state.movies.map((m) => (
-            <li key={m.id}>
-              <Link to={`/movies/${m.id}`}>{m.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
+    const { movies, isLoaded, error } = this.state;
+    if (error) {
+        return <div>Error: {error.message}</div>
+    }else if (!isLoaded) {
+      return <p>Loading....</p>
+    }else {
+      return (
+          <Fragment>
+            <h1>Choose a movie</h1>
+            <ul>
+              {movies.map((m) => (
+                  <li key={m.id}>
+                    <Link to={`/movies/${m.id}`}>{m.title}</Link>
+                  </li>
+              ))}
+            </ul>
+          </Fragment>
+      );
+    }
+
   }
 }
--- movies-app-frontend/src/components/OneMovie.js
+++ movies-app-frontend/src/components/OneMovie.js
@@ -1,36 +1,77 @@
 import React, { Component, Fragment } from 'react';
 
 export default class OneMovie extends Component {
-    state = { movie: {}}
+    state = { movie: {}, isLoaded: false, error: null}
 
     componentDidMount() {
-        this.setState({
-            movie: {
-                id: this.props.match.params.id,
-                title: "Some movie",
-                runtime: 150
-            }
-        })
+        fetch("http://localhost:4000/v1/movie/" + this.props.match.params.id)
+            .then((response) => {
+                if (response.status !== 200) {
+                    let err = Error;
+                    err.message = "Invalid response code " + response.status
+                    this.setState({error: err});
+                }
+
+                return response.json()
+            })
+            .then((json) => this.setState({
+                movie: json.movie,
+                isLoaded: true
+            },(error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }))
     }
 
     render() {
-        return (
-            <Fragment>
-                <h2>Movie: {this.state.movie.title}</h2>
-                <table className="table table-compact table-striped">
-                    <thead></thead>
-                    <tbody>
+        const {movie, isLoaded, error} = this.state
+        if (movie.genres) {
+            movie.genres = Object.values(movie.genres)
+        }else {
+            movie.genres = []
+        }
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <p>Loading....</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Movie: {movie.title} ({movie.year})</h2>
+                    <div className="float-start">
+                        <small>Rating: {movie.cbfc_rating}</small>
+                    </div>
+                    <div className="float-end">
+                        {movie.genres.map((m, index) => (
+                            <span className="badge bg-secondary me-1" key={index}>
+                                {m}
+                            </span>
+                        ))}
+                    </div>
+                    <div className="clearfix"></div>
+
+                    <hr />
+                    <table className="table table-compact table-striped">
+                        <thead></thead>
+                        <tbody>
                         <tr>
                             <td><strong>Title:</strong></td>
-                            <td>{this.state.movie.title}</td>
+                            <td>{movie.title}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Description:</strong></td>
+                            <td>{movie.description}</td>
                         </tr>
                         <tr>
                             <td><strong>Runtime:</strong></td>
-                            <td>{this.state.movie.runtime} minutes</td>
+                            <td>{movie.runtime} minutes</td>
                         </tr>
-                    </tbody>
-                </table>
-            </Fragment>
-        )
+                        </tbody>
+                    </table>
+                </Fragment>
+            )
+        }
     }
 }
