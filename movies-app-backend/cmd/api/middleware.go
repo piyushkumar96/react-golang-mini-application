9b5215ef912158371d3ccfc31ca1232173ba7c30
Piyush Kumar <iyu1kor@bosch.com>
2021-08-02 23:20:03 +0530

    set the cors policy to allow all required http methods

--- movies-app-backend/cmd/api/middleware.go
+++ movies-app-backend/cmd/api/middleware.go
@@ -5,6 +5,7 @@ import "net/http"
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 
 		next.ServeHTTP(w, r)
 	})
