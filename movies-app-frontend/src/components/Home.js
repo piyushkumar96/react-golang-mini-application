ef1dc1c7fcb58fccd05fe05b409677d2ad63734f
Piyush Kumar <iyu1kor@bosch.com>
2021-08-05 18:16:15 +0530

    Added image to home screen

--- movies-app-frontend/src/components/Home.js
+++ movies-app-frontend/src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-
+import "./Home.css"
 export default class Home extends Component {
     render() {
         return (
-            <h1>This is the home page</h1>
+            <div className="text-center">
+                <h1>This is the home page</h1>
+                <hr />
+                <div className="tickets"></div>
+            </div>
+
+
         )
     }
 }
