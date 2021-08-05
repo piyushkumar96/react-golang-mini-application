47be09c54bdeb8c07f13ec2dc75e82129ce6beab
Piyush Kumar <iyu1kor@bosch.com>
2021-08-05 22:52:20 +0530

    Added code for searching movie on basis of title

--- movies-app-frontend/src/components/GraphQL.js
+++ movies-app-frontend/src/components/GraphQL.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import Input from './form-components/Input';
 
 export default class GraphQL extends Component {
     constructor(props) {
@@ -11,8 +12,61 @@ export default class GraphQL extends Component {
             alert: {
                 type: "d-none",
                 message: "",
+            },
+            searchTerm: "",
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange = (evt) => {
+    let value = evt.target.value
+        this.setState(
+            (prevState) => ({
+                searchTerm: value
+            })
+        )
+        this.performSearch()
+    }
+
+    performSearch() {
+        const payload = `
+        {   
+            search(titleContains: "${this.state.searchTerm}") {
+                id
+                title
+                runtime
+                year
+                description
             }
         }
+        `
+
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json")
+
+        const requestOptions = {
+            method: "POST",
+            body: payload,
+            headers: headers
+        }
+
+        fetch("http://localhost:4000/v1/graphql", requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                let theList = Object.values(data.data.search)
+                return theList
+            })
+            .then((theList) => {
+                if (theList.length > 0) {
+                    this.setState({
+                        movies: theList
+                    })
+                }else {
+                    this.setState({
+                        movies: [],
+                    })
+                }
+            })
     }
 
     componentDidMount() {
@@ -56,6 +110,13 @@ export default class GraphQL extends Component {
             <Fragment>
                 <h2>GraphQL</h2>
                 <hr />
+                <Input
+                    title={"Search"}
+                    type={"text"}
+                    name={"search"}
+                    value={this.state.searchTerm}
+                    handleChange={this.handleChange}
+                    />
                 <div className="list-group">
                     {movies.map((m) => (
                         <a
